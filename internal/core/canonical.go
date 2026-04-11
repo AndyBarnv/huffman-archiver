6ea35040ac0d685eca2f7b5ad3e1f2ff37ecf5c0
Andrew_Baranov <andybaranov07@gmail.com>
2026-04-11 19:56:13 +0300

    feat: make canonical huffman coding functionality

---
+++ internal/core/canonical.go
@@ -0,0 +1,82 @@
+package core
+
+import "sort"
+
+type CodeInfo struct {
+	Char byte
+	Len  uint8
+	Code uint32
+}
+
+func getLengths(node *Node, length uint8, lengths map[byte]uint8) {
+	if node == nil {
+		return
+	}
+
+	if node.Left == nil && node.Right == nil {
+		if length == 0 {
+			length = 1
+		}
+		lengths[node.Char] = length
+		return
+	}
+
+	getLengths(node.Left, length+1, lengths)
+	getLengths(node.Right, length+1, lengths)
+}
+
+func BuildCanonicalCodes(root *Node) []CodeInfo {
+	lengthsMap := make(map[byte]uint8)
+	getLengths(root, 0, lengthsMap)
+
+	if len(lengthsMap) == 0 {
+		return nil
+	}
+
+	codes := make([]CodeInfo, 0, len(lengthsMap))
+	for char, length := range lengthsMap {
+		codes = append(codes, CodeInfo{Char: char, Len: length})
+	}
+
+	sort.Slice(codes, func(i, j int) bool {
+		if codes[i].Len != codes[j].Len {
+			return codes[i].Len < codes[j].Len
+		}
+		return codes[i].Char < codes[j].Char
+	})
+
+	code := uint32(0)
+	codes[0].Code = code
+	for i := 1; i < len(codes); i++ {
+		code++
+		if codes[i].Len > codes[i-1].Len {
+			code <<= (codes[i].Len - codes[i-1].Len)
+		}
+		codes[i].Code = code
+	}
+
+	return codes
+}
+
+func BuildDecocingTree(codes []CodeInfo) *Node {
+	root := &Node{}
+	for _, ci := range codes {
+		node := root
+		for i := uint8(0); i < ci.Len; i++ {
+			bit := (ci.Code >> (ci.Len - 1 - i)) & 1
+			if bit == 0 {
+				if node.Left == nil {
+					node.Left = &Node{}
+				}
+				node = node.Left
+			} else {
+				if node.Right == nil {
+					node.Right = &Node{}
+				}
+				node = node.Right
+			}
+		}
+		node.Char = ci.Char
+	}
+	return root
+}
