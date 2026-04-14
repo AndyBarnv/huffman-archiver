7cb60b807de1e56eb28b9e2785d0f5a3031bc648
Andrew_Baranov <andybaranov07@gmail.com>
2026-04-14 21:45:06 +0300

    refactor(archiver): add buffering to Compress function

--- internal/archiver/compress.go
+++ internal/archiver/compress.go
@@ -1,13 +1,12 @@
 package archiver
 
 import (
+	"bufio"
 	"encoding/binary"
 	"huffman-archiver/internal/bitio"
 	"huffman-archiver/internal/core"
 	"io"
 )
-
-const fileSignature = "HUF"
 
 func Compress(input io.Reader, output io.Writer) error {
 	data, err := io.ReadAll(input)
@@ -23,19 +22,22 @@ func Compress(input io.Reader, output io.Writer) error {
 	tree := core.BuildTree(freq)
 	codes := core.BuildCanonicalCodes(tree)
 
-	if _, err := output.Write([]byte(fileSignature)); err != nil {
+	bufWriter := bufio.NewWriter(output)
+	defer bufWriter.Flush()
+
+	if _, err := bufWriter.Write([]byte(core.FileSignature)); err != nil {
 		return err
 	}
 
 	buf8 := make([]byte, 8)
 	binary.LittleEndian.PutUint64(buf8, uint64(len(data)))
-	if _, err := output.Write(buf8); err != nil {
+	if _, err := bufWriter.Write(buf8); err != nil {
 		return err
 	}
 
 	buf2 := make([]byte, 2)
 	binary.LittleEndian.PutUint16(buf2, uint16(len(codes)))
-	if _, err := output.Write(buf2); err != nil {
+	if _, err := bufWriter.Write(buf2); err != nil {
 		return err
 	}
 
@@ -46,11 +48,11 @@ func Compress(input io.Reader, output io.Writer) error {
 		tableBuf[i*2] = ci.Char
 		tableBuf[i*2+1] = ci.Len
 	}
-	if _, err := output.Write(tableBuf); err != nil {
+	if _, err := bufWriter.Write(tableBuf); err != nil {
 		return err
 	}
 
-	writer := bitio.NewBitWriter(output)
+	writer := bitio.NewBitWriter(bufWriter)
 	for _, b := range data {
 		ci := codeMap[b]
 		for i := uint8(0); i < ci.Len; i++ {
